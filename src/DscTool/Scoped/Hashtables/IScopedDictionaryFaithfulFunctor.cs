4fbbd1512fa412515f94eb05b21bc2cfa9ce2b9a
nemonuri <nemonuri2@gmail.com>
2025-12-22 13:35:40 +0900

    ScopedPackedMapFaithfulFunctor 구현 완료

--- src/DscTool/Scoped/Hashtables/IScopedDictionaryFaithfulFunctor.cs
+++ src/DscTool/Scoped/Hashtables/IScopedDictionaryFaithfulFunctor.cs
@@ -2,63 +2,56 @@ using DscTool.Infrastructure;
 
 namespace DscTool.Scoped.Hashtables;
 
-public interface IScopedDictionaryConditionLifter<TCondition, TKey> :
-    IScopedHoareTripleMorphism<
-        Memory<KeyValuePair<TKey, TCondition>>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>, 
-        KeyValuePair<TKey, TCondition>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>>
+public interface IScopedPackedMapConditionLifter<TCondition, TKey> :
+    IScopedHoareTripleMorphism<PackedMap<TKey, TCondition>, TCondition, TCondition, TCondition>
     where TKey : IEquatable<TKey>
 {}
 
-public interface IScopedDictionaryLifter<T, TCondition, TKey> :
-    IScopedHoareTripleMorphism<
-        Memory<KeyValuePair<TKey, T>>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>, 
-        KeyValuePair<TKey, T>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>>
+public interface IScopedPackedMapLifter<T, TCondition, TKey> :
+    IScopedHoareTripleMorphism<PackedMap<TKey, T>, PackedMap<TKey, TCondition>, T, TCondition>
     where TKey : IEquatable<TKey>
 {}
 
-public interface IScopedDictionaryEmbedder<T, TCondition, TKey> :
-    IScopedHoareTripleMorphism<
-        KeyValuePair<TKey, T>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>, 
-        Memory<KeyValuePair<TKey, T>>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>>
+public interface IScopedPackedMapEmbedder<T, TCondition, TKey> :
+    IScopedHoareTripleMorphism<T, TCondition, PackedMap<TKey, T>, PackedMap<TKey, TCondition>>
     where TKey : IEquatable<TKey>
 {}
 
-public interface IScopedDictionaryFaithfulFunctor<T, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey> :
+public interface IScopedPackedMapFaithfulFunctor<T, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey> :
     IScopedFaithfulFunctor<
-        Memory<KeyValuePair<TKey, T>>, ReadOnlyDictionaryFallbackPair<TKey, TCondition>, 
-        T, TCondition, 
-        TCategory,
+        PackedMap<TKey, T>, PackedMap<TKey, TCondition>, T, TCondition, TCategory,
         TConditionLifter, TLifter, TEmbedder>
     where TCategory : IScopedCategory<T, TCondition>
-    where TConditionLifter : IScopedDictionaryConditionLifter<TCondition, TKey>
-    where TLifter : IScopedDictionaryLifter<T, TCondition, TKey>
-    where TEmbedder : IScopedDictionaryEmbedder<T, TCondition, TKey>
+    where TConditionLifter : IScopedPackedMapConditionLifter<TCondition, TKey>
+    where TLifter : IScopedPackedMapLifter<T, TCondition, TKey>
+    where TEmbedder : IScopedPackedMapEmbedder<T, TCondition, TKey>
     where TKey : IEquatable<TKey>
 {}
 
 
-public interface IScopedDictionaryConditionLifter<TAtomicCondition, TCondition, TKey> :
-    IScopedHoareTripleMorphism<Memory<TAtomicCondition>, TCondition, TCondition, TCondition>
+
+public interface IScopedPackedMapConditionLifter<TAtomicCondition, TCondition, TKey> :
+    IScopedHoareTripleMorphism<PackedMap<TKey, TAtomicCondition>, TCondition, TCondition, TCondition>
     where TKey : IEquatable<TKey>
 {}
 
-public interface IScopedDictionaryLifter<TAtom, T, TAtomicCondition, TCondition, TKey> :
-    IScopedHoareTripleMorphism<Memory<TAtom>, Memory<TAtomicCondition>, T, TCondition>
+public interface IScopedPackedMapLifter<TAtom, T, TAtomicCondition, TCondition, TKey> :
+    IScopedHoareTripleMorphism<PackedMap<TKey, TAtom>, PackedMap<TKey, TAtomicCondition>, T, TCondition>
     where TKey : IEquatable<TKey>
 {}
 
-public interface IScopedDictionaryEmbedder<TAtom, T, TAtomicCondition, TCondition, TKey> :
-    IScopedHoareTripleMorphism<T, TCondition, Memory<TAtom>, Memory<TAtomicCondition>>
+public interface IScopedPackedMapEmbedder<TAtom, T, TAtomicCondition, TCondition, TKey> :
+    IScopedHoareTripleMorphism<T, TCondition, PackedMap<TKey, TAtom>, PackedMap<TKey, TAtomicCondition>>
     where TKey : IEquatable<TKey>
 {}
 
-public interface IScopedDictionaryFaithfulFunctor<TAtom, T, TAtomicCondition, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey> :
+public interface IScopedPackedMapFaithfulFunctor<TAtom, T, TAtomicCondition, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey> :
     IScopedFaithfulFunctor<
-        Memory<TAtom>, Memory<TAtomicCondition>, T, TCondition, TCategory,
+        PackedMap<TKey, TAtom>, PackedMap<TKey, TAtomicCondition>, T, TCondition, TCategory,
         TConditionLifter, TLifter, TEmbedder>
     where TCategory : IScopedCategory<T, TCondition>
-    where TConditionLifter : IScopedDictionaryConditionLifter<TAtomicCondition, TCondition>
-    where TLifter : IScopedDictionaryLifter<TAtom, T, TAtomicCondition, TCondition>
-    where TEmbedder : IScopedDictionaryEmbedder<TAtom, T, TAtomicCondition, TCondition>
+    where TConditionLifter : IScopedPackedMapConditionLifter<TAtomicCondition, TCondition, TKey>
+    where TLifter : IScopedPackedMapLifter<TAtom, T, TAtomicCondition, TCondition, TKey>
+    where TEmbedder : IScopedPackedMapEmbedder<TAtom, T, TAtomicCondition, TCondition, TKey>
     where TKey : IEquatable<TKey>
 {}
---
+++ src/DscTool/Scoped/Hashtables/ScopedPackedMapFaithfulFunctor.cs
@@ -0,0 +1,112 @@
+using DscTool.Infrastructure;
+
+namespace DscTool.Scoped.Hashtables;
+
+public readonly struct ScopedPackedMapFaithfulFunctor<T, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey> :
+    IScopedPackedMapFaithfulFunctor<T, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey>
+    where TCategory : IScopedCategory<T, TCondition>
+    where TConditionLifter : IScopedPackedMapConditionLifter<TCondition, TKey>
+    where TLifter : IScopedPackedMapLifter<T, TCondition, TKey>
+    where TEmbedder : IScopedPackedMapEmbedder<T, TCondition, TKey>
+    where TKey : IEquatable<TKey>
+{
+    private readonly TConditionLifter _conditionLifter;
+    private readonly TLifter _lifter;
+    private readonly TEmbedder _embedder;
+    private readonly TCategory _category;
+
+    public ScopedPackedMapFaithfulFunctor(TConditionLifter conditionLifter, TLifter lifter, TEmbedder embedder, TCategory category)
+    {
+        _conditionLifter = conditionLifter;
+        _lifter = lifter;
+        _embedder = embedder;
+        _category = category;
+    }
+
+    [UnscopedRef] public ref readonly TCategory TargetCategory => ref _category;
+
+    [UnscopedRef] public ref readonly TConditionLifter ConditionLifter => ref _conditionLifter;
+
+    [UnscopedRef] public ref readonly TLifter Lifter => ref _lifter;
+
+    [UnscopedRef] public ref readonly TEmbedder Embedder => ref _embedder;
+
+    public bool Satisfies
+    (
+        scoped ref readonly PackedMap<TKey, TCondition> value, 
+        scoped ref readonly TCondition condition
+    )
+    {
+        if (!_category.IsSufficient(in value.Fallback, in condition)) {return false;}
+        foreach (var entry in value.AsSpan)
+        {
+            if (!_category.IsSufficient(in entry.Value, in condition)) {return false;}
+        }
+        return true;
+    }
+
+    public bool IsSufficient(scoped ref readonly TCondition sufficient, scoped ref readonly TCondition necessary)
+    {
+        return _category.IsSufficient(in sufficient, in necessary);
+    }
+}
+
+
+
+
+
+
+public readonly struct ScopedPackedMapFaithfulFunctor<TAtom, T, TAtomicCondition, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TAtomicConditionChecker, TKey> :
+    IScopedPackedMapFaithfulFunctor<TAtom, T, TAtomicCondition, TCondition, TCategory, TConditionLifter, TLifter, TEmbedder, TKey>
+    where TCategory : IScopedCategory<T, TCondition>
+    where TConditionLifter : IScopedPackedMapConditionLifter<TAtomicCondition, TCondition, TKey>
+    where TLifter : IScopedPackedMapLifter<TAtom, T, TAtomicCondition, TCondition, TKey>
+    where TEmbedder : IScopedPackedMapEmbedder<TAtom, T, TAtomicCondition, TCondition, TKey>
+    where TAtomicConditionChecker : IScopedConditionChecker<TAtomicCondition, TCondition>
+    where TKey : IEquatable<TKey>
+{
+    private readonly TCategory _category;
+    private readonly TConditionLifter _conditionLifter;
+    private readonly TLifter _lifter;
+    private readonly TEmbedder _embedder;
+    private readonly TAtomicConditionChecker _atomicConditionChecker;
+
+    public ScopedPackedMapFaithfulFunctor
+    (
+        TCategory category, 
+        TConditionLifter conditionLifter, 
+        TLifter lifter, 
+        TEmbedder embedder, 
+        TAtomicConditionChecker atomicConditionChecker
+    )
+    {
+        _category = category;
+        _conditionLifter = conditionLifter;
+        _lifter = lifter;
+        _embedder = embedder;
+        _atomicConditionChecker = atomicConditionChecker;
+    }
+
+    [UnscopedRef] public ref readonly TCategory TargetCategory => ref _category;
+
+    [UnscopedRef] public ref readonly TConditionLifter ConditionLifter => ref _conditionLifter;
+
+    [UnscopedRef] public ref readonly TLifter Lifter => ref _lifter;
+
+    [UnscopedRef] public ref readonly TEmbedder Embedder => ref _embedder;
+
+    public bool Satisfies(scoped ref readonly PackedMap<TKey, TAtomicCondition> value, scoped ref readonly TCondition condition)
+    {
+        if (!_atomicConditionChecker.Satisfies(in value.Fallback, in condition)) {return false;}
+        foreach (var entry in value.AsSpan)
+        {
+            if (!_atomicConditionChecker.Satisfies(in entry.Value, in condition)) {return false;}
+        }
+        return true;
+    }
+
+    public bool IsSufficient(scoped ref readonly TCondition sufficient, scoped ref readonly TCondition necessary)
+    {
+        return _category.IsSufficient(in sufficient, in necessary);
+    }
+}
