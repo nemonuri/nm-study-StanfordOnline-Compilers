7ecf855b55527fbacc05955214e5d6a4529734bd
nemonuri <nemonuri2@gmail.com>
2026-05-01 15:36:51 +0900

    'ProgramCode' 가 아니라, 'Program' 이라 한 이유가 있었네...

--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/Basic.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/Basic.lean
@@ -20,7 +20,7 @@ There are two major approaches to implementing programming languages
 inductive ImplementationApproach
   | compiler
   | interpreter
-  deriving DecidableEq, Hashable, Repr, BEq, ReflBEq, LawfulBEq
+
 
 /-!
 
@@ -28,24 +28,39 @@ inductive ImplementationApproach
 
 So, what does an interpreter do?
 
-1. You wrote a **Program code**(*code*).
+1. You wrote a **Program**(*prog*).
 2. You have a **Data**(*data*), whatever you want to run the program on.
-3. An **Interpreter**(*self*) takes as input, your program(*code*) and your data(*data*).
+3. An **Interpreter**(*self*) takes as input, your program(*prog*) and your data(*data*).
 4. It(*self*) produces the output directly.
-5. Meaning that it(*self*) doesn't do any processing of the program(*code*) before it executes it on the input.
-6. So you just write the program(*code*), and you invoke the interpreter(*self*) on the data(*data*), and the program(*code*) immediately begins running.
+5. Meaning that it(*self*) doesn't do any processing of the program(*prog*) before it executes it on the input.
+6. So you just write the program(*prog*), and you invoke the interpreter(*self*) on the data(*data*), and the program(*prog*) immediately begins running.
 
 -/
 
 
 structure ProgramCode (α: Type u) where
   val : α
-  deriving DecidableEq, Hashable, Repr, BEq, ReflBEq, LawfulBEq
+
 
 structure Data (α: Type u) where
   val : α
-  deriving DecidableEq, Hashable, Repr, BEq, ReflBEq, LawfulBEq
 
+
+inductive ProcessedState where
+  | pre
+  | post
+
+deriving instance DecidableEq, Hashable, Repr, BEq, ReflBEq, LawfulBEq
+  for ImplementationApproach, ProgramCode, Data, ProcessedState
+
+
+
+
+
+
+
+
+/-
 structure Processed (p: Prop) [Decidable p] : Prop where
   intro :: (processed: p)
 
@@ -56,23 +71,16 @@ instance instDecidableProcessed {p: Prop} [dp: Decidable p] : Decidable (Process
   with
   | .isTrue (h: p) => .isTrue (.intro h)
   | .isFalse (h: ¬p) =>
-    let notProcessed (h2: Processed p) : False :=
+    have notProcessed (h2: Processed p) : False :=
       match h2 with
       | .intro h2_1 => absurd h2_1 h
     .isFalse notProcessed
-
+-/
 
 
 
 end Nemonuri.Study.StanfordOnline.Compilers.Introduction
 
-instance optionToMonad : Monad Option where
-  pure x := some x
-  bind opt next :=
-    match opt with
-    | none => none
-    | some x => next x
 
-#print optionToMonad
 
 end --public
