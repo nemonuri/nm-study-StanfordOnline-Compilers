ff38ecf488775aeb473fd433ac4946886c1d7403
nemonuri <nemonuri2@gmail.com>
2026-05-01 15:14:34 +0900

    'instDecidableProcessed' 를 'Constructive' 하게 구현하는 데 성공!

--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/Basic.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/Basic.lean
@@ -32,30 +32,47 @@ So, what does an interpreter do?
 2. You have a **Data**(*data*), whatever you want to run the program on.
 3. An **Interpreter**(*self*) takes as input, your program(*code*) and your data(*data*).
 4. It(*self*) produces the output directly.
-5. Meaning that it(*self*) doesn't do any **Processing**(*pc*) of the program(*code*) before it executes it on the input.
+5. Meaning that it(*self*) doesn't do any processing of the program(*code*) before it executes it on the input.
 6. So you just write the program(*code*), and you invoke the interpreter(*self*) on the data(*data*), and the program(*code*) immediately begins running.
 
 -/
 
-section _1_1_Interpreter
 
-universe u
-variable (α: Type u) --[Hashable α] [Repr α] [BEq α] [LawfulBEq α]
-
-
-structure ProgramCode where
+structure ProgramCode (α: Type u) where
   val : α
   deriving DecidableEq, Hashable, Repr, BEq, ReflBEq, LawfulBEq
 
+structure Data (α: Type u) where
+  val : α
+  deriving DecidableEq, Hashable, Repr, BEq, ReflBEq, LawfulBEq
+
+structure Processed (p: Prop) [Decidable p] : Prop where
+  intro :: (processed: p)
+
+instance instDecidableProcessed {p: Prop} [dp: Decidable p] : Decidable (Processed p) :=
+  match
+    (motive := (Decidable p) → Decidable (Processed p))
+    dp
+  with
+  | .isTrue (h: p) => .isTrue (.intro h)
+  | .isFalse (h: ¬p) =>
+    let notProcessed (h2: Processed p) : False :=
+      match h2 with
+      | .intro h2_1 => absurd h2_1 h
+    .isFalse notProcessed
 
 
-
-
-
-
-end _1_1_Interpreter
 
 
 end Nemonuri.Study.StanfordOnline.Compilers.Introduction
+
+instance optionToMonad : Monad Option where
+  pure x := some x
+  bind opt next :=
+    match opt with
+    | none => none
+    | some x => next x
+
+#print optionToMonad
 
 end --public
