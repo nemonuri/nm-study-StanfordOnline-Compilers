67951504d795e2c006c60dae81a84e57d19f7a8b
nemonuri <nemonuri2@gmail.com>
2026-05-11 19:34:48 +0900

    ...나 DoesNotDoAnyProcessingBefore 제대로 정의한 거 맞냐...? - Prop 의 성질은 너무 헷갈려;;

--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/_1_1_Interpreter.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/_1_1_Interpreter.lean
@@ -12,12 +12,10 @@ set_option autoImplicit false
 
 namespace Nemonuri.Study.StanfordOnline.Compilers.Introduction
 
---open Program Runtime State
+universe us ul
+
 
 namespace Interpreter
-
-universe us ul
---variable {St: Type u} {La: Type v}
 
 /-!
 
@@ -57,23 +55,25 @@ def TakeAsInput (prog: Program.T St) (data: Data.T St) (_ st2: St) : Prop :=
   (prog ≠ 0) ∧ (Program.v st2 = prog) ∧ (data ≠ 0) ∧ (Data.v st2 = data)
 
 
+variable [DecidableEq (Program.T St)] [DecidableEq (Data.T St)]
+namespace TakeAsInput
 
-structure TakeAsInput.Label [DecidableEq (Program.T St)] [DecidableEq (Data.T St)] where
+structure Label [DecidableEq (Program.T St)] [DecidableEq (Data.T St)] where
   prog: Program.T St
   data: Data.T St
 
-variable [DecidableEq (Program.T St)] [DecidableEq (Data.T St)]
 
-instance TakeAsInput.Label.instDecidableEq : DecidableEq (TakeAsInput.Label St) :=
+instance Label.instDecidableEq : DecidableEq (Label St) :=
   fun l1 l2 =>
     if h: l1.prog = l2.prog ∧ l1.data = l2.data then
-      .isTrue (by simp [← TakeAsInput.Label.mk.injEq] at h; exact h)
+      .isTrue (by simp [← Label.mk.injEq] at h; exact h)
     else
-      .isFalse (by simp [← TakeAsInput.Label.mk.injEq] at h; exact h)
+      .isFalse (by simp [← Label.mk.injEq] at h; exact h)
 
-def TakeAsInput.toLTS (l: TakeAsInput.Label St) : Cslib.LTS St (TakeAsInput.Label St) :=
+def toLTS (l: Label St) : Cslib.LTS St (Label St) :=
   Cslib.LTS.Relation.toLTS (TakeAsInput St l.prog l.data) l
 
+end TakeAsInput
 
 
 /-!
@@ -89,8 +89,17 @@ variable
 def ProduceOutput (st1 st2: St) : Prop :=
   (Program.v st1 ≠ 0) ∧ (Data.v st1 ≠ 0) ∧ (Output.v st2 ≠ 0)
 
-structure ProduceOutput.Label where
-  deriving DecidableEq
+namespace ProduceOutput
+
+structure Label (St: Type us)
+
+instance Label.instDecidableEq : DecidableEq (Label St) :=
+  fun l1 l2 => .isTrue (by simp)
+
+def toLTS (l: Label St) : Cslib.LTS St (Label St) :=
+  Cslib.LTS.Relation.toLTS (ProduceOutput St) l
+
+end ProduceOutput
 
 
 
@@ -98,11 +107,87 @@ structure ProduceOutput.Label where
 5. Meaning that it(*self*) doesn't do any processing of the program(*prog*) before it executes it on the input.
 6. So you just write the program(*prog*), and you invoke the interpreter(*self*) on the data(*data*), and the program(*prog*) immediately begins running.
 -/
+namespace DoesNotDoAnyProcessingBefore
+
+open Std
+
+inductive Label where
+  | takeAsInput (l: TakeAsInput.Label St)
+  | produceOutput (l: ProduceOutput.Label St)
+  | other
+  deriving DecidableEq
+
+
+abbrev LabelList := List (Label St)
+
+namespace LabelList
+
+theorem length_aux {α} (ls: List α) : (ls ≠ []) ↔ (ls.length > 0) := by
+  constructor
+  case mp =>
+    intro not_nil
+    match meq1: ls with
+    | .cons hd tl => simp
+  case mpr =>
+    intro gt_0
+    match meq1: ls with
+    | .cons _ _ => simp
+
+abbrev NotNil (ls: LabelList St) : Prop := ls ≠ []
+
 /-
-def DoesNotDoAnyProcessingBefore (st1 st3: St) : Prop :=
-  (st2: St) → (prog: Program.T St) →
-  (data: Data.T St) → (TakeAsInput prog data st1 st2) → (ProduceOutput st2 st3) →
+def toMidRange ls (not_nil: NotNil St ls) : Roo (Fin (ls.length)) :=
+  Roo.mk
+    (Fin.mk 0 (Iff.mp (length_aux ls) not_nil))
+    (Fin.mk (ls.length-1) (by simp [length_aux] at not_nil; omega))
 -/
+
+variable (ls: LabelList St) (not_nil: NotNil St ls)
+
+abbrev IsHeadTakeAsInput : Prop := (ls.head not_nil) matches .takeAsInput _
+
+abbrev IsLastProduceOutput : Prop := (ls.getLast not_nil) matches .produceOutput _
+
+abbrev AllMidsOther : Prop := (ls.toSlice 1 (ls.length - 1)).toList.all (· matches .other)
+
+
+end LabelList
+
+/-
+protected def Def (lts: Cslib.LTS St (Label St)) : Prop :=
+  (st1: St) → (ls: List (Label St)) → (st2: St) →
+  (ls_not_nil: ls ≠ []) →
+  (ls_head_matches_takeAsInput: (ls.head ls_not_nil) matches .takeAsInput _) →
+  (ls_last_matches_produceOutput: (ls.getLast ls_not_nil) matches .produceOutput _) →
+-/
+
+end DoesNotDoAnyProcessingBefore
+
+open DoesNotDoAnyProcessingBefore LabelList in
+def DoesNotDoAnyProcessingBefore (lts: Cslib.LTS St (Label St)) : Prop :=
+  (st1: St) → (ls: List (Label St)) → (st2: St) → (mtr: lts.MTr st1 ls st2) →
+  (ils: List (Label St)) →
+  (List.IsInfix ils ls) →
+  (not_nil: NotNil St ils) →
+  (IsHeadTakeAsInput St ils not_nil) →
+  (IsLastProduceOutput St ils not_nil) →
+  (AllMidsOther St ils) →
+  (Program.v st1 = Program.v st2)
+
+
+
+/-
+def DoesNotDoAnyProcessingBefore (st1: St) (ls: List (Label St)) (st2: St) : Prop :=
+  match ls with
+  | [] => st1 = st2
+  | hd::tl =>
+  match hd with
+  | .takeAsInput (.mk prog data) =>
+-/
+
+
+
+
   --(prog: Program.T St) → (data: Data.T St) → (TakeAsInput prog data st1 st2) →
 
 /-
