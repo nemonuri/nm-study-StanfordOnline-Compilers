a25580d6f48827becd7159ec1ed360842f228e54
nemonuri <nemonuri2@gmail.com>
2026-05-12 19:16:13 +0900

    LabelMorph...음, 더 잘 정의된 것 같다!

--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/Basic.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/Basic.lean
@@ -1,6 +1,8 @@
 module
 
 public import Cslib.Foundations.Semantics.LTS.Basic
+public import Cslib.Foundations.Semantics.LTS.LTSCat.Basic
+public import Mathlib.Data.SetLike.Basic
 
 public section public_s
 @[expose] section expose_s
@@ -9,6 +11,7 @@ set_option autoImplicit false
 
 namespace Nemonuri.Study.StanfordOnline.Compilers
 
+/-
 universe u v
 
 structure State (St: Type u) where val: St
@@ -16,8 +19,42 @@ structure State (St: Type u) where val: St
 structure Label (La: Type v) where val: La
 
 def LTS (St: Type u) (La: Type v) := Cslib.LTS (State St) (Label La)
+-/
+
+universe us ul
+
+
+class LabelMorph (LabelA LabelB: Type _)
+  extends
+    SetLike LabelA (List (Option LabelB))
+
+
+/-
+instance (priority := low) (Label1 Label2: Type ul) : LabelMap Label1 Label2 where
+  labelMap _ := .none
+-/
+
+--def LabelMap.toMorphism {Label1 Label2 State} (inst: LabelMap Label1 Label2)
+/-
+def LabelMap.mapLTS {St} (La1 La2) [LabelMap La1 La2] (lts: Cslib.LTS St La1) : Cslib.LTS St (Option La2) :=
+  { Tr st1 la2? st2 :=
+      ∀(la1: La1), ((LabelMap.labelMap la1) = la2?) → (la2?.isSome) → (lts.Tr st1 la1 st2) }
+-/
 
 end Nemonuri.Study.StanfordOnline.Compilers
+
+/-
+open Nemonuri.Study.StanfordOnline.Compilers in
+def Cslib.LTS.mapByLabelMap {St La1} (lts: Cslib.LTS St La1) La2 [LabelMap La1 La2] : Cslib.LTS St (Option La2) :=
+  { Tr st1 la2? st2 :=
+      ∀(la1: La1), ((LabelMap.labelMap la1) = la2?) → (la2?.isSome) → (lts.Tr st1 la1 st2) }
+-/
+
+open Nemonuri.Study.StanfordOnline.Compilers in
+def Cslib.LTS.mapBySetLike {St LaB} (ltsB: Cslib.LTS St LaB) LaA [LabelMorph LaA LaB] : Cslib.LTS St LaA :=
+  { Tr st1 la st2 := ∀lb, (lb ∈ SetLike.coe la) → ltsB.withIdle.MTr st1 lb st2 }
+
+
 
 end expose_s
 end public_s
--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/_1_1_Interpreter.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/_1_1_Interpreter.lean
@@ -1,9 +1,11 @@
 module
 
 --public import Std.Data.ExtDHashMap
---public import Nemonuri.Study.StanfordOnline.Compilers.Introduction.Program
+public import Nemonuri.Study.StanfordOnline.Compilers.Introduction.Basic
 public import Cslib.Foundations.Semantics.LTS.Basic
 public import Cslib.Foundations.Semantics.LTS.Relation
+public import Cslib.Foundations.Semantics.LTS.LTSCat.Basic
+--public import Mathlib.Data.Set.Defs
 
 public section public_s
 @[expose] section expose_s
@@ -16,6 +18,7 @@ universe us ul
 
 
 namespace Interpreter
+section _1
 
 /-!
 
@@ -95,14 +98,13 @@ structure ProduceOutput (st1 st2: St) : Prop where
 
 namespace ProduceOutput
 
-inductive Directly.LabelView where
-  | other
+inductive Directly.Label where
   | produceOutput
   deriving DecidableEq
 
-def Directly (lts: Cslib.LTS St Directly.LabelView) : Prop :=
+def Directly (lts: Cslib.LTS St (Option Directly.Label)) : Prop :=
   ∀st1 st2,
-    (lts.Tr st1 .produceOutput st2) →
+    (lts.Tr st1 (.some .produceOutput) st2) →
     (ProduceOutput St st1 st2)
 
 end ProduceOutput
@@ -110,16 +112,16 @@ end ProduceOutput
 /-!
 5. Meaning that it(*self*) doesn't do any processing of the program(*prog*) before it executes it on the input.
 -/
-inductive DoesNotDoAnyProcessing.LabelView where
+inductive DoesNotDoAnyProcessing.Label where
   | takeAsInput (prog: Program.T St)
   | produceOutput
 
-structure DoesNotDoAnyProcessing (lts: Cslib.LTS St (DoesNotDoAnyProcessing.LabelView St)) : Prop where
-  takeAsInput_spec prog data st1 st2 : (lts.Tr st1 (.takeAsInput prog) st2) → (TakeAsInput St prog data st1 st2)
-  produceOutput_spec st1 st2 : (lts.Tr st1 .produceOutput st2) → (ProduceOutput St st1 st2)
+structure DoesNotDoAnyProcessing (lts: Cslib.LTS St (Option (DoesNotDoAnyProcessing.Label St))) : Prop where
+  takeAsInput_spec prog data st1 st2 : (lts.Tr st1 (.some (.takeAsInput prog)) st2) → (TakeAsInput St prog data st1 st2)
+  produceOutput_spec st1 st2 : (lts.Tr st1 (.some .produceOutput) st2) → (ProduceOutput St st1 st2)
   main_spec prog (st1 st2 st3: St) :
-    (lts.Tr st1 (.takeAsInput prog) st2) →
-    (lts.Tr st2 .produceOutput st3) →
+    (lts.Tr st1 (.some (.takeAsInput prog)) st2) →
+    (lts.Tr st2 (.some .produceOutput) st3) →
     ((prog = (Program.v st2)) ∧ ((Program.v st2) = (Program.v st3)))
 
 /-!
@@ -135,33 +137,123 @@ structure InvokeOnTheData (data: Data.T St) (st1 st2: St) : Prop where
   post: (Program.v st2 = Program.v st1) ∧ (Data.v st2 = data)
 
 
-inductive ImmediatelyBeginsRunning.LabelView where
-  | other
+inductive ImmediatelyBeginsRunning.Label where
   | invokeOnTheData
+  deriving DecidableEq
 
 class IsRunning where
   v: (Program.T St) → Bool
 
 variable [IsRunning St]
 
-structure ImmediatelyBeginsRunning (lts: Cslib.LTS St ImmediatelyBeginsRunning.LabelView) : Prop where
-  lts_spec (st1 st2: St) (data: Data.T St) : (lts.Tr st1 .invokeOnTheData st2) → (InvokeOnTheData St data st1 st2)
+structure ImmediatelyBeginsRunning (lts: Cslib.LTS St (Option ImmediatelyBeginsRunning.Label)) : Prop where
+  lts_spec (st1 st2: St) (data: Data.T St) : (lts.Tr st1 (.some .invokeOnTheData) st2) → (InvokeOnTheData St data st1 st2)
   main_spec (st1 st2: St) :
-    (lts.Tr st1 .invokeOnTheData st2) →
+    (lts.Tr st1 (.some .invokeOnTheData) st2) →
     (IsRunning.v (Program.v st1) = false ∧ IsRunning.v (Program.v st2) = true)
 
 
 /-!
 7. we can say that the interpreter(*self*) is, *is online*, meaning it the work that it does is all part of running your program.
 -/
-inductive IsOnline.LabelView where
-  | other
+inductive IsOnline.Label where
   | partOfRunningYourProgram
+  deriving DecidableEq
 
-def IsOnline (lts: Cslib.LTS St IsOnline.LabelView) : Prop := ∀st1 st2, (lts.Tr st1 .partOfRunningYourProgram st2)
+def IsOnline (lts: Cslib.LTS St (Option IsOnline.Label)) : Prop := ∀st1 st2, (lts.Tr st1 (.some .partOfRunningYourProgram) st2)
+
+end _1
+
+section def_s
+
+protected class abbrev State.Program (St: Type us) := Program St, Zero (Program.T St)
+
+protected class abbrev State.Data (St: Type us) := Data St, Zero (Data.T St)
+
+protected class abbrev State.Output (St: Type us) := Output St, Zero (Output.T St)
+
+class State (St: Type us) extends
+  toStateProgram: State.Program St,
+  toStateData: State.Data St,
+  toStateOutput: State.Output St
+
+--abbrev LawfulState (St: Type us) [State St] [Zero (Program.T St)] [Zero (Data.T St)] [Zero (Output.T St)] := State St
+
+
+variable (St: Type us) [State St]
+
+inductive Label where
+  | writeTheProgram (prog: Program.T St)
+  | invokeOnTheData (data: Data.T St)
+  | takeAsInput (prog: Program.T St) (data: Data.T St)
+  | produceOutput
+
+
+instance : LabelSetLike (Option ProduceOutput.Directly.Label) (Label St) where
+  coe la :=
+    match la with
+    | .some .produceOutput => setOf (· matches .produceOutput)
+    | .none => setOf (fun x => !(x matches .produceOutput))
+  coe_injective' := by
+    have lemma1 : Nonempty (Label St) := Nonempty.intro (.produceOutput)
+    intro la1 la2 h1
+    simp at h1
+    match meq1: la1, meq2: la2 with
+    | .none, .none => rfl
+    | .some la1s, .some la2s => congr
+    | .none, .some _ | .some _, .none =>
+      simp at h1
+      unfold setOf Set at h1
+      rewrite [funext_iff] at h1
+      simp at h1
+
+/-
+instance : LabelMap (Label St) (DoesNotDoAnyProcessing.Label St) where
+  labelMap l :=
+    match l with
+    | .takeAsInput prog _ => .some (.takeAsInput prog)
+    | _ => .none
+
+instance : LabelMap (Label St) (ImmediatelyBeginsRunning.Label) where
+  labelMap l :=
+    match l with
+    | .invokeOnTheData _ => .some (.invokeOnTheData)
+    | _ => .none
+
+instance : LabelMap (Label St) (IsOnline.Label) where
+  labelMap _ := .some (.partOfRunningYourProgram)
+-/
+
+
+def mkLTS : Cslib.LTS St (Label St) where
+  Tr st1 μ st2 :=
+    match μ with
+    | .takeAsInput prog data => TakeAsInput St prog data st1 st2
+    | .produceOutput => ProduceOutput St st1 st2
+    | .writeTheProgram prog => WriteTheProgram St prog st1 st2
+    | .invokeOnTheData data => InvokeOnTheData St data st1 st2
 
 
 
+attribute [local simp] mkLTS Cslib.LTS.mapBySetLike
+
+protected theorem ProduceOutput.Directly.proof
+  : (mkLTS St).mapBySetLike (Option ProduceOutput.Directly.Label) |> ProduceOutput.Directly St := by
+    simp [ProduceOutput.Directly]
+    intro st1 st2 h1
+    replace h1 := h1 .produceOutput
+    simp at h1
+    apply h1
+    simp [Membership.mem, Set.Mem, SetLike.coe, setOf]
+
+#print ProduceOutput.Directly.proof
+
+
+
+
+
+
+end def_s
 
 end Interpreter
 
