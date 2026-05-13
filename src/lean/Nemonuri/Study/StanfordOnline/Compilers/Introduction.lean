1498c73b1b8b7e6a480e1b22d43288f79587168f
nemonuri <nemonuri2@gmail.com>
2026-05-13 18:58:18 +0900

    빌드도 성공!

--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction.lean
@@ -2,7 +2,8 @@
 
 module
 
-public import Nemonuri.Study.StanfordOnline.Compilers.Introduction.Program
+public import Nemonuri.Study.StanfordOnline.Compilers.Introduction.Basic
 public import Nemonuri.Study.StanfordOnline.Compilers.Introduction._1_Intro_to_Compilers
+public import Nemonuri.Study.StanfordOnline.Compilers.Introduction._1_1_Interpreter
 --public import Nemonuri.Study.StanfordOnline.Compilers.Introduction._1_Intro_to_Compilers
 --public import Nemonuri.Study.StanfordOnline.Compilers.Introduction._1_1_Interpreter
