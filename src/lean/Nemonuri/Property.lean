e07174e7d8742a58bcfe95b64d4c00baa48640bd
nemonuri <nemonuri2@gmail.com>
2026-05-17 14:48:09 +0900

    mk_iff...이런 편리한 매크로가 있었다니!

--- src/lean/Nemonuri/Property.lean
+++ src/lean/Nemonuri/Property.lean
@@ -1,5 +1,6 @@
 module
 
+public import Mathlib.Tactic.MkIffOfInductiveProp
 
 @[expose] public section public_s
 
@@ -13,11 +14,14 @@ namespace Nemonuri
 
 --namespace Property
 
+@[mk_iff]
 class ZeroEq {σ T: Type u} [Zero σ] [Zero T] (f: σ → T) : Prop where
   zero_eq: (f 0 = 0)
 
+
 variable {σ T: Type u} [Zero σ] [Zero T] (f: σ → T)
 
+/-
 @[simp]
 theorem zeroEq_iff : (ZeroEq f) ↔ (f 0 = 0) := by
   constructor
@@ -25,11 +29,13 @@ theorem zeroEq_iff : (ZeroEq f) ↔ (f 0 = 0) := by
     obtain ⟨h'⟩ := h ; exact h'
   · intro h
     constructor ; exact h
+-/
 
+@[mk_iff]
 class SourceExists : Prop where
   source_exists (v: T) (h: v ≠ 0) : ∃(s: σ), (f s = v)
 
-
+/-
 omit [Zero σ] in
 @[simp]
 theorem sourceExists_iff : (SourceExists f) ↔ ((v: T) → (v ≠ 0) → ∃(s: σ), (f s = v)) := by
@@ -38,10 +44,16 @@ theorem sourceExists_iff : (SourceExists f) ↔ ((v: T) → (v ≠ 0) → ∃(s:
     obtain ⟨h'⟩ := h ; exact h'
   · intro h
     constructor ; exact h
+-/
 
-
+@[mk_iff]
 class abbrev IsProperty : Prop := ZeroEq f, SourceExists f
 
+
+
+theorem IsProperty.imp_and (self: IsProperty f) : ZeroEq f ∧ SourceExists f := (isProperty_iff f).mp self
+
+/-
 @[simp]
 theorem isProperty_iff : (IsProperty f) ↔ (ZeroEq f) ∧ (SourceExists f) := by
   constructor
@@ -51,7 +63,7 @@ theorem isProperty_iff : (IsProperty f) ↔ (ZeroEq f) ∧ (SourceExists f) := b
   · intro h
     have lm1 : IsProperty f := @IsProperty.mk _ _ _ _ f h.left h.right
     exact lm1
-
+-/
 
 
 end Nemonuri
