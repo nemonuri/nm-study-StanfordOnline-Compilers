0bc238b9f64b7737386ca78ffabb1fc56ce9b731
nemonuri <nemonuri2@gmail.com>
2026-05-24 17:37:04 +0900

    증명....완료! - 아....내가 하고자 하는 게 이거였구나...!!!!

--- src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/_1_2_Compiler.lean
+++ src/lean/Nemonuri/Study/StanfordOnline/Compilers/Introduction/_1_2_Compiler.lean
@@ -11,6 +11,7 @@ public import Mathlib.Data.Subtype
 public import Mathlib.Logic.ExistsUnique
 public import Mathlib.Tactic.Lift
 public import Nemonuri.Builder
+public import Mathlib.Data.List.Pairwise
 
 
 public section public_s
@@ -205,9 +206,13 @@ instance : DecidableEq (Output.T St) := Output.v.deq
 namespace WillProduceTheOutput
 
 @[reducible]
-def Req (st1: St) : Prop := (IsExecutableRunning _ st1) ∧ (Output.v st1 = 0)
+def Req (st1: St) : Prop := (IsExecutableRunning _ st1) --∧ (Output.v st1 = 0)
 
-def Ens (st1 st2: St) : Prop := (AppEq Executable.v st1 st2) ∧ (Output.v st2 ≠ 0)
+def Ens (st1 st2: St) : Prop :=
+  (¬IsExecutableRunning _ st1) ∧
+  (AppEq YourProgram.v st1 st2) ∧
+  (AppEq Executable.v st1 st2) ∧
+  (Output.v st2 ≠ 0)
 
 end WillProduceTheOutput
 
@@ -285,24 +290,37 @@ the program
 -/
 namespace CanRunSameMany
 
-def ReqPre (st1: St) : Prop := (Executable.v st1 ≠ 0)
+def ReqData (dataF dataL: Data.T St) : Prop := (dataF ≠ 0) ∧ (dataL ≠ 0) ∧ (dataF ≠ dataL)
 
-def ReqAll (st1 stX: St) : Prop := (Executable.v st1 = Executable.v stX)
+def ReqFirst (dataF: Data.T St) (stF: St) : Prop := (Data.v stF = dataF) ∧ (IsExecutableRunning _ stF)
 
-def ReqPost (st2: St) : Prop := (Data.v st2 ≠ 0) ∧ (IsExecutableRunning _ st2)
+def EnsAll (stX1 stX2: St) : Prop := (AppEq Executable.v stX1 stX2) ∧ (AppEq YourProgram.v stX1 stX2)
 
-def ReqAny (st2 stX: St) : Prop := (Data.v stX ≠ 0) ∧ (IsExecutableRunning _ st2) ∧ (Data.v stX ≠ Data.v st2)
+def EnsLast (dataL: Data.T St) (stL: St) : Prop := (Data.v stL = dataL) ∧ (IsExecutableRunning _ stL)
+
+instance : IsTrans St (EnsAll _) where
+  trans stF stM stL := by --h_fm h_ml
+    simp [EnsAll, appEq_iff]
+    intro _ _ _ _
+    constructor <;> simp [*]
 
 end CanRunSameMany
 
 open CanRunSameMany in
 def CanRunSameMany {La} (lts: Cslib.LTS St La) : Prop :=
+  ∀dataF dataL, (ReqData _ dataF dataL) →
+  ∀stF, (ReqFirst _ dataF stF) →
+  ∃ls stL sts, (lts.Execution stF ls stL sts) ∧
+    (sts.Pairwise (EnsAll _)) ∧
+    (EnsLast _ dataL stL)
+--  ∀(dataF dataL: Data.T St), (dataF ≠ 0) → (data2 ≠ 0) → (data1 ≠ data2) →
+/-
   ∀(st1: St), (ReqPre _ st1) →
   ∃(st2: St), (ReqPost _ st2) ∧
   ∃sts μs, (List.Forall (ReqAll _ st1) sts) ∧
             (∃stX ∈ sts, ReqAny _ st2 stX) ∧
             (lts.Execution st1 μs st2 sts)
-
+-/
 
 /-
 def CanRunSameMany {La} (lts: Cslib.LTS St La) : Prop :=
@@ -473,6 +491,12 @@ theorem beginRun_exists (st1: St) (req: BeginRun.Req _ st1)
   exists req
   apply Subtype.property
 
+theorem willProduceTheOutput_exists (st1: St) (req: WillProduceTheOutput.Req _ st1)
+  : ∃st2, (instLTS _).Tr st1 (.willProduceTheOutput) st2 ∧ WillProduceTheOutput.Ens _ st1 st2 := by
+  simp [instLTS]
+  exists req
+  apply Subtype.property
+
 set_option pp.proofs true in
 theorem CanRunSeparately.proof : CanRunSeparately St (instLTS _) := by
   simp [CanRunSeparately]
@@ -517,244 +541,28 @@ theorem IsOffline.proof : IsOffline St (instLTS _) := by
   intro sts_elem_exe_zero
   simp [sts_elem_exe_zero]
   rfl
-/-
-  intro data data_p st1 req_pre stL req_last sts sts_ne sts_head_eq sts_last_eq req_all --data data_p --sts sts_p st2 st2_p
-  --conv at req_all => rw [IsOffline.ReqAll]
-  --unfold IsOffline.ReqAll at req_all ; simp [appEq_iff] at req_all
-  simp [IsOffline.ReqFirst] at req_pre
-  simp [IsOffline.ReqLast, IsExecutableRunning] at req_last
-  --simp [IsOffline.ReqPre] at req_pre
-  obtain ⟨_,_,_,_⟩ := req_pre
-  --simp [IsOffline.ReqAll, appEq_iff]
-  --simp [IsOffline.ReqPost, IsExecutableRunning]
-  --simp [IsOffline.EnsAll, IsExecutableRunning]
-  obtain ⟨st2, ⟨st2_tr, st2_ens⟩⟩ := produceExecutable_exists _ st1 (by simp; trivial)
-  simp [ProduceExecutable.Ens, RunningStateEq, appEq_iff] at st2_ens
-  obtain ⟨⟨_,_⟩,⟨_,_⟩⟩ := st2_ens
-  have st2_lm1 : IsOffline.ReqAll _ st1 st2 := by simp [IsOffline.ReqAll, appEq_iff, *]
-  obtain ⟨st3, ⟨st3_tr, st3_ens⟩⟩ := takeData_exists St data st2 (by simp; exact data_p)
-  simp [TakeData.Ens, appEq_iff] at st3_ens
-  obtain ⟨_,_,_,_⟩ := st3_ens
-  have st3_lm1 : IsOffline.ReqAll _ st2 st3 := by simp [IsOffline.ReqAll, appEq_iff, *]
-  have stL_intro (req1: BeginRun.Req _ st3) (req2: IsOffline.ReqAll _ st3 stL)
-    : (instLTS _).Tr st3 (.beginRun) stL := by
-    simp [instLTS]
-    exists req1
-    simp only [Subtype.coe_eq_iff]
-    have lm1: BeginRun.Ens St st3 stL := by
-      simp [BeginRun.Ens, appEq_iff]
-      simp [BeginRun.Req] at req1
-      obtain ⟨_,_,_,_⟩ := req1
-      simp [IsOffline.ReqAll, appEq_iff] at req2
-      constructorm* _ ∧ _ <;> simp_all
-      --simp_all
-    --simp [BeginRun.Req] at req1
-    --obtain ⟨_,_,_,_⟩ := req1
-    --simp [IsOffline.ReqAll, appEq_iff] at req2
-    --simp_all [instLTS]
--/
 
-/-
-  obtain ⟨st4, ⟨st4_tr, st4_ens⟩⟩ := beginRun_exists St st3 (by
-    subst data
-    simp_all
+attribute [simp] CanRunSameMany.EnsAll appEq_iff in
+theorem CanRunSameMany.proof : CanRunSameMany St (instLTS _) := by
+  simp [CanRunSameMany]
+  rintro dataF dataL ⟨r1,r2,r3⟩ stF ⟨r4,r5⟩
+  --simp [IsExecutableRunning] at r5
+  --unfold CanRunSameMany.EnsAll
+  --simp [appEq_iff]
+  simp [CanRunSameMany.EnsLast]
+  obtain ⟨st1,st1_tr,_,_,_,_⟩ := willProduceTheOutput_exists _ stF (by simp [WillProduceTheOutput.Req] ; exact r5)
+  simp only [appEq_iff] at *
+  --have ea_st1 : CanRunSameMany.EnsAll _ stF st1 := by simp [*]
+  obtain ⟨st2,st2_tr,_,_,_,_⟩ := takeData_exists _ dataL st1 (by simp [TakeData.Req] ; exact r2)
+  simp only [appEq_iff] at *
+  --have ea_st2 : CanRunSameMany.EnsAll _ st1 st2 := by simp [*]
+  obtain ⟨st3,st3_tr,_,_,_,_,_⟩ := beginRun_exists _ st2 (by
     simp [BeginRun.Req]
-    constructorm* _ ∧ _ <;> trivial
-  )
-  simp [BeginRun.Ens, appEq_iff] at st4_ens
-  obtain ⟨_,_,_,_,_⟩ := st4_ens
-  --simp [h1, h2, h3] at * ; clear h1 h2 h3
--/
-/-
-  have lm1 := Execution.refl (instLTS _) st4 |>.stepL st4_tr |>.stepL st3_tr |>.stepL st2_tr
-  obtain ⟨sts_head, sts_tail, sts_eq⟩ := List.ne_nil_iff_exists_cons.mp sts_ne
-  have lm2 : (∃ x, (instLTS St).Execution st1 x stL sts) := by
-    simp_all
--/
-
-  --simp_all
-  --MTr.single _ st2_tr |>.stepR _ st3_tr |>.stepR _ st4_tr
-  --subst_eqs
-  --simp [List.forall_iff_forall_mem, IsOffline.ReqAll, appEq_iff] at sts_p
-  --simp [List.forall_iff_forall_mem, IsOffline.EnsAll, IsExecutableRunning]
-/-
-  simp [IsOffline.ReqPre] at req_pre
-  obtain ⟨_,_,_,_⟩ := req_pre
-  simp [IsOffline.ReqPost, IsExecutableRunning]
-  obtain ⟨st2, ⟨st2_tr, st2_ens⟩⟩ := produceExecutable_exists _ st1 (by simp; trivial)
-  simp [ProduceExecutable.Ens, RunningStateEq, appEq_iff] at st2_ens
-  obtain ⟨⟨_,_⟩,⟨_,_⟩⟩ := st2_ens
-  simp_all
--/
-
-
-
-
-
-  --obtain ⟨st2, ⟨st2_tr, st2_ens⟩⟩ := (TakeData.req_iff _ data st1).mp req.left
-  --simp [TakeData.Ens] at st2_ens
-  /-
-  have lm_st3_1 (h: Executable.v st2 = 0) := (ProduceExecutable.req_iff _ st2).mp (by
-    obtain ⟨h1_1, h1_2, h1_3⟩ := req
-    obtain ⟨h2_1, h2_2, h2_3⟩ := st2_ens
-    simp [ProduceExecutable.Req] ; rw [← h2_2] ; simp only [h1_2]
-    simp [IsYourProgramRunning, IsNonZeroRunning] at h1_3
-    simp_all
-  )
-  -/
-/-
-    if h: (Executable.v st2 = 0) then
-      st2
-    else
-      st2
--/
-/-
-  have lm_st3 := (BeginRun.req_iff _ st2).mp (by
-    simp [BeginRun.Req]
-    constructor
-    · rw [← st2_ens.left] ; exact req.left
-    · constructor
-      · simp [IsYourProgramRunning, IsNonZeroRunning] at req
-  )
--/
-
-
-    --simp [BeginRun.Req]
-    --conv => arg 1; rw [eq_self_iff_true]; tactic => have := And.intro req.left st2_ens.left;
-  --obtain ⟨st3, ⟨st3_tr, st3_ens⟩⟩ := (BeginRun.req_iff _ st1).mp req.left
-  --obtain ⟨st2, st2_tr⟩ := (TakeData.req_iff _ data st1).mp req.left |> isImageNonempty_iff.mp
-
-
-
-  --have lm1 : (instLTS _).IsImageNonempty st1 (.takeData data) := by
-  --  simp [isImageNonempty_iff, instLTS]
-/-
-    match h1: Ability.toTakeData.dreq data with
-    | .isTrue _ => simp
-    | .isFalse cont =>
-      simp [TakeData.Req] at cont
-      have := req.left
-      contradiction
--/
-  --simp [isImageNonempty_iff] at lm1
-  --obtain ⟨st2, st2_p⟩ := isImageNonempty_iff.mp lm1
-/-
-    have req1 : TakeData.Req St data := by simp [TakeData.Req] ; exact req.left
-    exists (Ability.toTakeData.run data st1 req1).val
-    simp
--/
-
-  --intro data st1 _ _ _ stX _ _ _ _ _
-  --simp_all only [not_false_eq_true, forall_const]
-
-
-
-/-
-open Cslib LTS in
-attribute [simp]
-  CanReach IsNonZeroRunning IsYourProgramRunning IsExecutableRunning RunningStateEq
-  Nemonuri.zeroEq_iff Nemonuri.sourceExists_iff Cslib.LTS.isImageNonempty_iff
-in
-example (data: Data.T St) (st1: St) (h: ∃st2, (RunSeparately _ data st1 st2))
-  : (instLTS _).CanReach st1 h.choose := by
-  rename (State St) => st
-  have lm1 := h.choose_spec
-  simp [runSeparately_iff, RunSeparately.Post] at lm1
-  obtain ⟨⟨lm1_1, lm1_2⟩, _, _, _, _⟩ := lm1
-  by_cases h1: st1 = h.choose
-  · simp [←h1]; exists []; exact MTr.refl
-  · obtain ⟨st3, st3_p⟩ := st.toStateData.source_exists _ lm1_1
-    have lm2 : (instLTS _).IsImageNonempty st1 (.takeData data) := by
-      simp [instLTS, RunSeparately.takeData_iff]
-  --obtain ⟨st3, st3_p⟩ := st.toStateProgram.toSourceExists.source_exists _ lm1_2
-  --unfold Exists.choose
-  --rename (State St) => st
--/
-/-
-  obtain ⟨lm_prog1, lm_prog2⟩ := st.toStateProgram.toIsProperty.imp_and
-  obtain ⟨lm_data1, lm_data2⟩ := st.toStateData.toIsProperty.imp_and
-  obtain ⟨lm_exe1, lm_exe2⟩ := st.toStateExecutable.toIsProperty.imp_and
-  simp [runSeparately_iff]  at h
-  let (eq := h_eq) ⟨hw, hw_p⟩ := h
-  simp at lm_prog1 lm_prog2 lm_data1 lm_data2 lm_exe1 lm_exe2
-  simp [runSeparately_iff] at hw_p
--/
-/-
-  simp [runSeparately_iff] at *
-  obtain ⟨⟨h1, ⟨_, _⟩⟩, ⟨_, _, ⟨_, _, _, _⟩⟩⟩ := h
-  have lm1 : ∃st3, (instLTS _).Tr st1 (.takeData data) st3 ∧ (instLTS _).Tr st3 (.beginRun) st2 := by
-    have lm1_1 : (instLTS _).IsImageNonempty st1 (.takeData data) := by
-      simp [instLTS, RunSeparately.takeData_iff]
-
-  suffices goal: ∃stg, ((instLTS _).CanReach st1 stg) ∧ (ProduceExecutable.Post _ st1 stg) ∧ (Not <| IsExecutableRunning _ st2) from by
-    simp [ProduceExecutable.Post] at goal
-    obtain ⟨stg, ⟨stg_mtr, ⟨⟨_, h2⟩, _⟩⟩⟩ := goal
-    simp [ExecutableIsProgram.zero_iff] at h2
-    have lm2 : (instLTS _).IsImageNonempty stg (.takeData data) := by
-      simp [instLTS, RunSeparately.takeData_iff]
-      simp [*]
-      --exists ((lm_data2 data h1) |> Classical.choose)
-    obtain ⟨st4, st4_tr⟩ := lm2 |> isImageNonempty_iff.mp
-  by_cases h8: Executable.v st1 = 0
-  · have lm1 : (instLTS _).IsImageNonempty st1 .produceExecutable := by
-      simp [instLTS, produceExecutable_iff]
-      constructor <;> constructor <;> trivial
-    obtain ⟨st3, st3_tr⟩ := lm1 |> isImageNonempty_iff.mp
-    clear lm1
-    have lm2 : (instLTS _).IsImageNonempty st3 (.takeData data) := by
-      simp [instLTS, RunSeparately.takeData_iff]
-      constructor <;> constructor <;> trivial
-    obtain ⟨st4, st4_tr⟩ := lm2 |> isImageNonempty_iff.mp
-    clear lm2
-    have lm3 : (instLTS _).Tr st4 .beginRun st2 := by
-      simp [instLTS, RunSeparately.beginRun_iff]
-      constructor <;> constructor <;> trivial
-    simp only [← MTr.single_iff] at st3_tr
-    have lm4 := MTr.stepR (instLTS _) st3_tr st4_tr |> (MTr.stepR (instLTS _) · lm3)
-    simp at lm4
-    exists [Label.produceExecutable, Label.takeData data, Label.beginRun]
-    --have lm2 : (instLTS _).Tr st1 .produceExecutable lm1_st := by
-    --  simp [Membership.mem, Set.Mem] at lm1_p
-
-  · sorry
-      --by_contra cont
-      --simp at cont
--/
-/-
-    exists [.produceExecutable, .takeData data, .beginRun]
-    have lm1 : (instLTS _).image st1 .produceExecutable |> Set.Nonempty := by
-      simp [instLTS, image, Set.Nonempty]
-      constructor
-      · constructor <;> constructor
-        · exact h2.left
-        · exact h6
-        ·
--/
-    --rw [← List.singleton_append]
-    --rw [MTr.split_iff]
-    --let (eq := h7) image1 := (instLTS _).image st1 .produceExecutable
-    --simp [instLTS, image, setOf] at h7
-
-
-
-/-
-    let w : List (Label St) := [.takeData data, .beginRun]
-    exists w
-    have lm2 : w = ([.takeData data] ++ [.beginRun]) := by subst w ; simp
-    subst w ; rw [lm2]
-    apply (MTr.split_iff (instLTS _)).mpr
-    simp [MTr.single_iff]
-    simp [instLTS]
-    open RunSeparately in
-    constructor
-    · constructor <;> constructor <;> constructor
-      · exact h1
-      · constructor
--/
-
-
-    --apply (MTr.split_iff (instLTS _)).mpr
-
+    constructorm* _ ∧ _ <;> trivial)
+  simp only [appEq_iff] at *
+  --have ea_st3 : CanRunSameMany.EnsAll _ st2 st3 := by simp [*]
+  have := Execution.refl (instLTS _) st3 |>.stepL st3_tr |>.stepL st2_tr |>.stepL st1_tr
+  exists [Label.willProduceTheOutput, Label.takeData dataL, Label.beginRun]
 
 
 
